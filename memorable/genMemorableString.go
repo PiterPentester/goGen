7439540aeccd51f79835f608a4a096d74350ddb9
Bohdan Pakhomov <shtormless@gmail.com>
2020-05-30 18:23:08 +0300

    memorable: fix anti-pattern (nil check on []string)

--- memorable/genMemorableString.go
+++ memorable/genMemorableString.go
@@ -1,10 +1,11 @@
 package memorable
 
 import (
-	"net/http"
 	"errors"
 	"io/ioutil"
+	"net/http"
 	"strings"
+
 	"github.com/PiterPentester/goGen/abracadabra"
 )
 
@@ -12,78 +13,79 @@ var client http.Client
 
 // ErrBadCode - handle not "OK" http status code
 var ErrBadCode = errors.New("bad status code")
+
 // ErrEmptyResult - handle empty result for parsing functions
 var ErrEmptyResult = errors.New("function return empty result")
 
 // CurlToAddr - makes a curl request to the site & save the output as string
 func CurlToAddr(addr string) (string, error) {
-    resp, err := client.Get(addr)
-    if err != nil {
-	    return "", err
-    }
-    defer resp.Body.Close()
-    
-    if resp.StatusCode != http.StatusOK {
-        return "", ErrBadCode
-    }
-    
-    bodyBytes, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return "", err
-    }
-    
-    bodyString := string(bodyBytes)
-    return bodyString, nil
+	resp, err := client.Get(addr)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", ErrBadCode
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	bodyString := string(bodyBytes)
+	return bodyString, nil
 }
 
 // getTextOut - cut text after "text_out" and store it to txt var
 func getTextOut(txt string) (string, error) {
 	if txt == "" || len(txt) == 0 {
-	    return "", ErrEmptyResult
+		return "", ErrEmptyResult
 	}
 	// text begins after "text_out"
 	f := "text_out"
 	i := strings.Index(txt, f)
 	if i > -1 {
-	    txt = txt[i+len(f)+3:len(txt)-2]
+		txt = txt[i+len(f)+3 : len(txt)-2]
 	}
 	return txt, nil
 }
 
 // splitByTag - split our txt var by "<p>", remove ".<\/p>\r" from res and return list of lines.
 func splitByTag(txt string) []string {
-    // split txt by <p> tag
+	// split txt by <p> tag
 	res := strings.Split(txt, "<p>")
 	for id, elem := range res {
 		// remove ".<\/p>\r" from res
 		i := strings.Index(elem, ".")
 		if i > -1 {
 			elem = elem[:i]
-	    }
-	    res[id] = elem
+		}
+		res[id] = elem
 	}
 	return res
 }
 
 // splitBySpace - split our list of lines into a list of words and return it.
 func splitBySpace(res []string) ([]string, error) {
-    var words []string
-    
-    if res == nil || len(res) == 0 {
-	    return words, ErrEmptyResult
+	var words []string
+
+	if len(res) == 0 {
+		return words, ErrEmptyResult
 	}
-	
+
 	// split res by " "
 	for _, e := range res {
-	    w := strings.Split(e, " ")
-	    for _, word := range w {
+		w := strings.Split(e, " ")
+		for _, word := range w {
 			if len(word) < 3 {
-			    continue
+				continue
 			}
-		    words = append(words, word)
+			words = append(words, word)
 		}
 	}
-    return words, nil
+	return words, nil
 }
 
 // parseOutput - wrapper for parsing functions, return list of words
@@ -91,46 +93,46 @@ func parseOutput() ([]string, error) {
 	var words []string = nil
 	txt, err := CurlToAddr("http://www.randomtext.me/api/gibberish")
 	if err != nil {
-	    return words, err
+		return words, err
 	}
-	
-    txt, err = getTextOut(txt)
-    if err != nil {
-	    return words, err
+
+	txt, err = getTextOut(txt)
+	if err != nil {
+		return words, err
 	}
-	
-    res := splitByTag(txt)
-    if res == nil || len(res) == 0 {
-	    return words, ErrEmptyResult
+
+	res := splitByTag(txt)
+	if res == nil || len(res) == 0 {
+		return words, ErrEmptyResult
 	}
-	
-    words, err = splitBySpace(res)
-    if err != nil {
-	    return words, err
+
+	words, err = splitBySpace(res)
+	if err != nil {
+		return words, err
 	}
-	
+
 	return words, nil
 }
 
-// GetRandWords - get number of wanted words & create list of random choosen words with predefined length  
-// GetRandWords(3, words) => [word1, word2, word3] 
+// GetRandWords - get number of wanted words & create list of random choosen words with predefined length
+// GetRandWords(3, words) => [word1, word2, word3]
 func GetRandWords(numOfWords int) []string {
 	words, _ := parseOutput()
-    res := make([]string, numOfWords)
-    for i := range res {
+	res := make([]string, numOfWords)
+	for i := range res {
 		res[i] = strings.Title(words[abracadabra.SeededRand.Intn(len(words))])
 	}
 	return res
 }
 
-// GenMemorablePass 
+// GenMemorablePass
 func GenMemorablePass(words []string) string {
-    res := ""
-    for _, w := range words {
+	res := ""
+	for _, w := range words {
 		smbl := abracadabra.Symbols[abracadabra.SeededRand.Intn(len(abracadabra.Symbols))]
-	    res += w + string(smbl)
+		res += w + string(smbl)
 	}
 	d := abracadabra.Digits[abracadabra.SeededRand.Intn(len(abracadabra.Digits))]
 	res += string(d)
-	return res 
+	return res
 }
